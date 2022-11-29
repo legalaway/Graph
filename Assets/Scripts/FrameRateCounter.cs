bfcfb34efc4c886a81a6f7c6cb8326a54342068a
Dimon <pop.your.heart.01@yandex.ru>
2022-11-29 23:02:04 +0300

    Measuring Performance

---
+++ Assets/Scripts/FrameRateCounter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FrameRateCounter : MonoBehaviour
+{
+    public enum DisplayMode {FPS, MS }
+
+
+    [SerializeField] private TextMeshProUGUI _display;
+    [SerializeField, Range(0.1f, 2f)] private float _sampleDuration = 1f;
+    [SerializeField] private DisplayMode _displayMode = DisplayMode.FPS;
+
+    private int _frames;
+    private float _duration;
+    private float _worstDuration;
+    private float _bestDuration = float.MaxValue;
+
+    private void Update()
+    {
+        float frameDuration = Time.unscaledDeltaTime;
+        _frames += 1;
+        _duration += frameDuration;
+
+        if (frameDuration < _bestDuration)
+        {
+            _bestDuration = frameDuration;
+        }
+        if (frameDuration > _worstDuration)
+        {
+            _worstDuration = frameDuration;
+        }
+        if (_duration >= _sampleDuration)
+        {
+            if (_displayMode == DisplayMode.FPS)
+            {
+                _display.SetText("FPS\n{0:0}\n{1:0}\n{2:0}", 1f / _bestDuration, _frames / _duration, 1f / _worstDuration);
+            }
+            else
+            {
+                _display.SetText("MS\n{0:1}\n{1:1}\n{2:1}", 1000f * _bestDuration,  _duration / _frames, 1000f * _worstDuration);
+            }
+            _duration = 0f;
+            _frames = 0;
+            _bestDuration = float.MaxValue;
+            _worstDuration = 0f;
+        }
+
+    }
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,12 +1,22 @@
+using System;
 using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
+    public enum TransitionMode { Cycle, Random }
+
     [SerializeField] private Transform _pointPrefab;
     [SerializeField, Range(10, 100)] private int _resolution = 10;
     [SerializeField] private FunctionLibrary.FunctionName _function;
+    [SerializeField, Min(0f)] private float _functionDuration = 1f;
+    [SerializeField, Min(0f)] private float _transitionDuration = 1f;
+    [SerializeField] private TransitionMode _transitionMode;
+
 
     private Transform[] _points;
+    private float _duration;
+    private bool _transitioning;
+    private FunctionLibrary.FunctionName _transitionFunction;
 
     private void Awake()
     {
@@ -23,6 +33,42 @@ public class Graph : MonoBehaviour
 
     private void Update()
     {
+        _duration += Time.deltaTime;
+        if (_transitioning)
+        {
+            if (_duration >= _transitionDuration)
+            {
+                _duration -= _transitionDuration;
+                _transitioning = false;
+            }
+        }
+        else if (_duration > _functionDuration)
+        {
+            _duration -= _functionDuration;
+            _transitioning = true;
+            _transitionFunction = _function;
+            PickNextFunction();
+        }
+
+        if (_transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateFunction();
+        }
+    }
+
+    private void PickNextFunction()
+    {
+        _function = _transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(_function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(_function);
+    }
+
+    private void UpdateFunction()
+    {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(_function);
         float time = Time.time;
         float step = 2f / _resolution;
@@ -37,6 +83,27 @@ public class Graph : MonoBehaviour
             }
             float u = (x + 0.5f) * step - 1f;
             _points[i].localPosition = f(u, v, time);
+        }
+    }
+
+    private void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function from = FunctionLibrary.GetFunction(_transitionFunction);
+        FunctionLibrary.Function to = FunctionLibrary.GetFunction(_function);
+        float progress = _duration / _transitionDuration;
+        float time = Time.time;
+        float step = 2f / _resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < _points.Length; i++, x++)
+        {
+            if (x == _resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            _points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
         }
     }
 }
